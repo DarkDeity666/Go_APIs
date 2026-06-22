55c7f19250885c92b7ee7339be5fe1b79c956750
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-06-22 22:13:15 +0530

    inital setup for gin support

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,53 +1,21 @@
-package config
+package config 
 
 import (
-	"flag"
-	"log"
 	"os"
-
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
-
-type HTTPServer struct {
-	Addr string `yaml:"address"`
-}
-
-// env-default: "production"
-
-
 type Config struct{
-	Env string `yaml:"env" env:"ENV" env-required:"true"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"` 
+	Port string
+	MongoURI string
+	RedisAddr string
+	JWTSecret string 
+	JWTRefreshSecret string
+
+	CloudinaryCloudName string
+	CloudinaryAPIKey string
+	CloudinaryAPISecret string
 }
 
-
-func MustLoad() *Config {
-	var configPath string
-
-	configPath = os.Getenv("CONFIG_PATH")
-
-	if configPath == "" {
-		flags := flag.String("config", "", "path to the configuration file")
-		flag.Parse()
-		configPath = *flags
-
-		if configPath == "" {
-			log.Fatal("Config path is not set")
-		}
-	}
-
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("Config file does not exist : %s", configPath)
-	}
-
-	var cfg Config
-
-	err := cleanenv.ReadConfig(configPath, &cfg)
-
-	if err != nil {
-		log.Fatalf("can not read config file: %s", err.Error())
-	}
-	return &cfg
+func Load()(*config,error){
 
 }
