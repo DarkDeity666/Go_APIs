e3786ce5a102412caf292907e6bace7d8fb88044
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-06-29 06:17:36 +0530

    Feat: Register function using gin and go standard library bcrypt

---
+++ internal/utils/password.utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+
+//hashing password
+func HashPassword(password string)(string, error){
+	hashedPassword,err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	
+	if err != nil{
+		return "",err
+	}
+	return string(hashedPassword),nil
+}
+
+func CheckPassword(password, hash string) bool {
+	err:= bcrypt.CompareHashAndPassword(
+		[]byte(hash),
+		[]byte(password),
+	)
+
+	return err == nil
+}
